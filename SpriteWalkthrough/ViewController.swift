98316ef4f9dacd9a396aae650c0e92eab0161861
k-andy <andy.sh182@gmail.com>
2016-10-26 13:44:08 +0300

    Initial Commit

---
+++ SpriteWalkthrough/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpriteWalkthrough
+//
+//  Created by Andriy Shvets on 10/26/16.
+//  Copyright © 2016 Andriy Shvets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
